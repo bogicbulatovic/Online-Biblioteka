566a636e2b77f7822d2617a277972afb2bdb5c3f
Bogi <bulatovicbogic99@gmail.com>
2022-08-22 21:54:15 +0200

    remove unecesary array creation

--- js/podesavanja.js
+++ js/podesavanja.js
@@ -1,16 +1,14 @@
 const btnsContainer = document.getElementById("tab_buttons_container");
 
-const btnsCollection = btnsContainer.querySelectorAll("button");
+const tabButtons = btnsContainer.querySelectorAll("button");
 
-const btnsArray = Array.from(btnsCollection);
-
-btnsArray.forEach((btnEl) => {
+tabButtons.forEach((btnEl) => {
   const handleClick = (e) => {
     const clickedBtn = e.target;
 
     clickedBtn.classList.add("active_button");
 
-    btnsArray.forEach((btnEl) => {
+    tabButtons.forEach((btnEl) => {
       if (btnEl !== clickedBtn) {
         btnEl.classList.remove("active_button");
       }
