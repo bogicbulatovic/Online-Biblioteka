8ef06becc005afe4ee971d780841f8415536b5ed
Bogi <bulatovicbogic99@gmail.com>
2022-08-22 21:34:36 +0200

    fix broken sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,17 @@
-let isOpen = false;
+let isSidebarOpen = false;
 
 const handleBtnClick = () => {
   const navbar = document.querySelector(".navbar");
 
   const openClass = "navbar-open";
 
-  if (isMenuOpen) {
+  if (isSidebarOpen) {
     navbar.classList.remove(openClass);
 
-    isMenuOpen = false;
+    isSidebarOpen = false;
   } else {
     navbar.classList.add(openClass);
-    isMenuOpen = true;
+    isSidebarOpen = true;
   }
 };
 
